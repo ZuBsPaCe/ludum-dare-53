61f3ceb41629b4cd518e9c32d1f30046f660257c
ZuBsPaCe <kurt.rudin@gmx.net>
2023-04-29 22:55:05 +0200

    Storing procedural city data in city scene and loading it properly.

--- Game/Scripts/City.cs
+++ Game/Scripts/City.cs
@@ -1,6 +1,5 @@
 using Godot;
 using Godot.Collections;
-using System.IO;
 
 [Tool]
 public partial class City : Node3D
@@ -33,20 +32,67 @@ public partial class City : Node3D
 	private Array<PackedScene> _sceneBuildings;
 
 
+	[Export]
+	private Godot.Collections.Dictionary<string, Variant> _levelData;
+
 
 	[Export]
-	public bool Generate
+	public bool GenerateDebug
 	{
 		get { return false; }
 		set
 		{
 			if (value)
-				RunGenerate();
+				RunGenerate(true);
 		}
 	}
 
-    private void RunGenerate()
+
+    [Export]
+    public bool GenerateRelease
     {
+        get { return false; }
+        set
+        {
+            if (value)
+                RunGenerate(false);
+        }
+    }
+
+	private Map<TileType, Tile> _map;
+
+
+    public override void _Ready()
+    {
+		if (Engine.IsEditorHint())
+		{ 
+			return; 
+		}
+
+        var root = GetNode<Node3D>(_genPath);
+        foreach (var child in root.GetChildren())
+            child.QueueFree();
+
+
+		if (_levelData == null || _levelData.Count == 0)
+			return;
+
+        var tileTypeArray = (Array<int>)_levelData["TileTypeArray"];
+
+        _map = new((int)_levelData["MapWidth"], (int)_levelData["MapHeight"], TileType.Building);
+
+        for (int index = 0; index < _map.Size; ++index)
+        {
+            _map.SetTypeIndex(index, (TileType)tileTypeArray[index]);
+        }
+
+        GenerateTiles(root, _map);
+    }
+
+    private void RunGenerate(bool debug)
+    {
+		GD.Print($"Running Generate. Debug: [{debug}]");
+
 		var root = GetNode<Node3D>(_genPath);
 		foreach (var child in root.GetChildren())
 			child.QueueFree();
@@ -56,9 +102,8 @@ public partial class City : Node3D
 		for (int ySection = 0; ySection < _ySectionCount; ++ySection)
 			for (int xSection = 0; xSection < _xSectionCount; ++xSection)
 			{
-				GenerateSection(map, root, xSection, ySection);
+				GenerateSection(map, xSection, ySection);
 			}
-
 
 		Array<int> tileTypeArray = new();
 
@@ -67,60 +112,96 @@ public partial class City : Node3D
 			tileTypeArray.Add((int) map.GetTypeIndexed(index));
         }
 
-        Dictionary<string, Variant> levelData = new()
-        {
-            { "TileTypeArray", tileTypeArray },
-            { "MapWidth", map.Width },
-            { "MapHeight", map.Height }
-        };
+		_levelData = new();
+		
+		_levelData["TileTypeArray"] = tileTypeArray;
+		_levelData["MapWidth"] = map.Width;
+		_levelData["MapHeight"] = map.Height;
 
-        string levelText = Json.Stringify(levelData);
-		File.WriteAllText("Level.json", levelText);
+		if (debug)
+		{
+			GenerateTiles(root, map);
+		}
     }
 
-	private void GenerateSection(Map<TileType, Tile> map, Node3D root, int xSection, int ySection)
+	private void GenerateSection(Map<TileType, Tile> map, int xSection, int ySection)
 	{
 		for (int yIndex = 0; yIndex < _sectionTileSize; ++yIndex)
 		{
 			int yTile = (ySection * _sectionTileSize + yIndex);
-            int y = yTile * _tileSize;
+            
 
 			for (int xIndex = 0; xIndex < _sectionTileSize; ++xIndex)
 			{
 				int xTile = xSection * _sectionTileSize + xIndex;
-                int x = xTile * _tileSize;
 
 				TileType tileType;
-				Node3D tile;
-				
+
 				if (yIndex == 0 && xIndex == 0)
                 {
 					tileType = TileType.StreetCrossing;
-                    tile = _sceneStreetCrossing.Instantiate<Node3D>();
 				}
 				else if (yIndex == 0)
 				{
 					tileType = TileType.StreetHor;
-                    tile = _sceneStreetHor.Instantiate<Node3D>();
 				}
 				else if (xIndex == 0)
 				{
 					tileType = TileType.StreetVer;
-                    tile = _sceneStreetVer.Instantiate<Node3D>();
 				}
 				else
 				{
 					tileType = TileType.Building;
-                    tile = _sceneBuildings[0].Instantiate<Node3D>();
 				}
 
 				map.SetType(xTile, yTile, tileType);
 				map.SetItem(xTile, yTile, new Tile());
+			}
+		}
+    }
+
+	private void GenerateTiles(Node3D root, Map<TileType, Tile> map)
+	{
+		for (int yTile = 0; yTile < map.Height; ++yTile)
+		{
+            int y = yTile * _tileSize;
+
+            for (int xTile = 0; xTile < map.Width; ++xTile)
+			{
+                int x = xTile * _tileSize;
+
+				TileType tileType = map.GetType(xTile, yTile);
+
+                Node3D tile;
+
+                switch (tileType)
+                {
+                    case TileType.Building:
+                        tile = _sceneBuildings[0].Instantiate<Node3D>();
+                        break;
+
+                    case TileType.StreetVer:
+                        tile = _sceneStreetVer.Instantiate<Node3D>();
+                        break;
+
+                    case TileType.StreetHor:
+                        tile = _sceneStreetHor.Instantiate<Node3D>();
+                        break;
+
+                    case TileType.StreetCrossing:
+                        tile = _sceneStreetCrossing.Instantiate<Node3D>();
+                        break;
+
+                    default:
+                        Debug.Fail($"Unknown TileType [{tileType}]");
+						continue;
+                }
+
+                map.SetItem(xTile, yTile, new Tile());
 
                 tile.Position = new Vector3(x, 0, y);
                 root.AddChild(tile);
-                tile.Owner = this;
-			}
-		}
+            }
+        }
     }
 }
--- Game/Scripts/Game.cs
+++ Game/Scripts/Game.cs
@@ -3,11 +3,14 @@ using System;
 
 public partial class Game : Node3D
 {
+    [Export] private PackedScene _sceneCity;
+
     private City _city;
 
     public override void _Ready()
     {
-        _city = GetNode<City>("City");
+        _city = _sceneCity.Instantiate<City>();
+        AddChild(_city);
     }
 
     public void CloseGameAndFree()

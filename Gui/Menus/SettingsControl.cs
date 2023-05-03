fc8c0cbfea18e9d8e115818ba8f446d50e7e7843
ZuBsPaCe <kurt.rudin@gmx.net>
2023-05-03 12:11:30 +0200

    Fixed settings buttons

--- Gui/Menus/SettingsControl.cs
+++ Gui/Menus/SettingsControl.cs
@@ -1,23 +1,58 @@
 using Godot;
+using System;
 
 public partial class SettingsControl : MenuControlBase
 {
-    private Button _musicMinusButton;
+    HSlider _musicSlider;
+    HSlider _soundSlider;
 
     public override void _Ready()
     {
-        _musicMinusButton = GetNode<Button>("%MusicMinusButton");
-        var musicSlider = GetNode<HSlider>("%MusicSlider");
-        var soundSlider = GetNode<HSlider>("%SoundSlider");
+        Button musicMinusButton = GetNode<Button>("%MusicMinusButton");
+        Button musicPlusButton = GetNode<Button>("%MusicPlusButton");
+        Button soundMinusButton = GetNode<Button>("%SoundMinusButton");
+        Button soundPlusButton = GetNode<Button>("%SoundPlusButton");
+
+        _musicSlider = GetNode<HSlider>("%MusicSlider");
+        _soundSlider = GetNode<HSlider>("%SoundSlider");
         var fullscreenCheckBox = GetNode<CheckBox>("%FullscreenCheckBox");
 
-        musicSlider.Value = UserSettings.MusicVolume;
-        soundSlider.Value = UserSettings.SoundVolume;
+        _musicSlider.Value = UserSettings.MusicVolume;
+        _soundSlider.Value = UserSettings.SoundVolume;
         fullscreenCheckBox.ButtonPressed = UserSettings.Fullscreen;
 
-        musicSlider.ValueChanged += (value) => Music_ValueChanged(value);
-        soundSlider.ValueChanged += (value) => Sound_ValueChanged(value);
+        _musicSlider.ValueChanged += (value) => Music_ValueChanged(value);
+        _soundSlider.ValueChanged += (value) => Sound_ValueChanged(value);
         fullscreenCheckBox.Toggled += (value) => Fullscreen_Toggled(value);
+
+        musicMinusButton.Pressed += MusicMinusButton_Pressed;
+        musicPlusButton.Pressed += MusicPlusButton_Pressed;
+        soundMinusButton.Pressed += SoundMinusButton_Pressed;
+        soundPlusButton.Pressed += SoundPlusButton_Pressed;
+    }
+
+    private void SoundPlusButton_Pressed()
+    {
+        UserSettings.SoundVolume = Mathf.Clamp(UserSettings.SoundVolume + 0.025f, 0, 1);
+        _soundSlider.Value = UserSettings.SoundVolume;
+    }
+
+    private void SoundMinusButton_Pressed()
+    {
+        UserSettings.SoundVolume = Mathf.Clamp(UserSettings.SoundVolume - 0.025f, 0, 1);
+        _soundSlider.Value = UserSettings.SoundVolume;
+    }
+
+    private void MusicPlusButton_Pressed()
+    {
+        UserSettings.MusicVolume = Mathf.Clamp(UserSettings.MusicVolume + 0.025f, 0, 1);
+        _musicSlider.Value = UserSettings.MusicVolume;
+    }
+
+    private void MusicMinusButton_Pressed()
+    {
+        UserSettings.MusicVolume = Mathf.Clamp(UserSettings.MusicVolume - 0.025f, 0, 1);
+        _musicSlider.Value = UserSettings.MusicVolume;
     }
 
     private void Fullscreen_Toggled(bool value)
@@ -28,6 +63,7 @@ public partial class SettingsControl : MenuControlBase
     private void Sound_ValueChanged(double value)
     {
         UserSettings.SoundVolume = (float) value;
+        Sounds.PlaySound(SoundType.Crash);
     }
 
     private void Music_ValueChanged(double value)

6b7525e38dacbd9bf91304156aeb41d78885cccf
ZuBsPaCe <kurt.rudin@gmx.net>
2023-05-04 09:43:06 +0200

    Smoother follow camera

--- Game/Scripts/FollowCamera.cs
+++ Game/Scripts/FollowCamera.cs
@@ -11,17 +11,32 @@ public partial class FollowCamera : Camera3D
 
 	private Node3D _target;
 
+	private Vector3 _currentLookAtFloor;
+	private float _currentLookAtHeight;
+
 	public override void _Ready()
 	{
 		_target = GetNode<Node3D>(_targetPath);
-	}
+        _currentLookAtFloor = new Vector3(_target.GlobalTransform.Origin.X, 0, _target.GlobalTransform.Origin.Z) + _target.GlobalTransform.Basis.Z * 5;
+		_currentLookAtHeight = _target.GlobalTransform.Origin.Y;
+    }
 
     public override void _PhysicsProcess(double delta)
     {
 		var targetGlobalTransform = _target.GlobalTransform.TranslatedLocal(_offset);
 		targetGlobalTransform.Origin.Y = _fixedHeight;
+
 		GlobalTransform = GlobalTransform.InterpolateWith(targetGlobalTransform, (float)(_smoothing * delta));
 
-		LookAt(_target.GlobalTransform.Origin, Vector3.Up);
+
+		Vector3 lookAtFloor = new Vector3(_target.GlobalTransform.Origin.X, 0, _target.GlobalTransform.Origin.Z) + _target.GlobalTransform.Basis.Z * 5;
+		_currentLookAtFloor = _currentLookAtFloor.Lerp(lookAtFloor, (float)(_smoothing * delta));
+
+        float lookAtHeight = _target.GlobalTransform.Origin.Y;
+		_currentLookAtHeight = Mathf.Lerp(_currentLookAtHeight, lookAtHeight, (float)(0.1 * delta));
+
+		Vector3 lookAt = new Vector3(_currentLookAtFloor.X, _currentLookAtHeight, _currentLookAtFloor.Z);
+
+        LookAt(lookAt, Vector3.Up);
     }
 }
